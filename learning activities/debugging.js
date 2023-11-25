4e013a92a64013d7ac7ea0ee4b0c70e5b999bb2c
Dereckr <dereckromos@gmail.com>
2023-11-24 23:54:34 -0400

    changes

--- learning activities/debugging.js
+++ learning activities/debugging.js
@@ -7,20 +7,70 @@
 // area = radius * radius * PI;
 // console.log("Area2:", area);
 
-const PI = 3.14;
-let area = 0;
-let radius= 0;
+// const PI = 3.14;
+// let area = 0;
+// let radius= 0;
 
 
-function circleArea(radius) {
-let area = radius * radius * PI;
-return area;
-}
+// function circleArea(radius) {
+// let area = radius * radius * PI;
+// return area;
+// }
 
-area = circleArea(3);
-console.log("Area:3", area);
+// area = circleArea(3);
+// console.log("Area:3", area);
 
-area = circleArea(4);
-console.log("Area2:", area);
+// area = circleArea(4);
+// console.log("Area2:", area);
+
+// let i = 0;
+// let n = 0;
+// while (i < 5) {
+//   i++;
+//   if (i === 3) {
+//     continue;
+//   }
+//   n += i;
+//   console.log(n);
+// }
 
 
+// const DAYS = 6;
+// const LIMIT = 30;
+// let studentReport = [11, 42, 33, 64, 29, 37, 44];
+
+// for (let i=0;i<studentReport.length; i++){
+
+//     let value = studentReport[i]
+//     if (value <30){
+//     console.log(value)
+// }}
+
+// let i = 0;
+// while (i<studentReport.length){
+//     if (studentReport[i]< 30){
+//         console.log(studentReport[i]);
+//     }
+//     i++; 
+// }
+
+// studentReport.forEach(function(item){
+//     if (item <LIMIT){console.log(item);}
+// })
+
+// for(const i in studentReport){
+//     if (studentReport[i]<LIMIT){
+//         console.log(studentReport[i]);
+//     }
+// }
+
+function calculate(a, b, callback) {
+    callback(a + b);
+    return result;
+  }
+
+  function displayResult(result) {
+    console.log('The result is: ' + result);
+  }
+
+  calculate(2, 3,displayResult);
---
+++ scripts/w04-task.js
@@ -0,0 +1,62 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+let myProfile = {
+
+    name : "Dereck Romero Moscoso",
+    photo: "../w02-task/images/photo.jpeg",
+    favoriteFoods:["Mondongo", "Fricase", "Sushi", "Valdiviano", "Tonkatsu", "Frijol Tropeiro", "Acai", "Lasagna", "Tempura"],
+    hobbies:["play wally", "play soccer", "board games", "sudoku", "watch movies"],
+    placesLived:[]
+};
+
+
+
+
+/* Populate Profile Object with placesLive objects */
+
+myProfile.placesLived.push(
+    {
+        place: "La Paz, Bolivia", 
+        length:"28 years"
+    }
+    );
+myProfile.placesLived.push(
+    {
+        place: "Maceio, Brasil", length:"2 years"});
+myProfile.placesLived.push({place: "Fukushima, Japan", length:"4 years"});
+
+
+/* DOM Manipulation - Output */
+
+/* Name */
+const profileName = document.querySelector("#name");
+profileName.textContent = myProfile.name;
+/* Photo with attributes */
+const photo=document.querySelector("#photo")
+photo.setAttribute("src", myProfile.photo);
+photo.setAttribute("alt", myProfile.name);
+
+/* Favorite Foods List*/
+myProfile.favoriteFoods.forEach(food => {let li=document.createElement("li");
+li.textContent=food;
+document.querySelector("#favorite-foods").appendChild(li); 
+});
+
+/* Hobbies List */
+myProfile.hobbies.forEach(hobby=>{let li=document.createElement("li");
+li.textContent=hobby;
+document.querySelector("#hobbies").appendChild(li);
+})
+
+/* Places Lived DataList */
+myProfile.placesLived.forEach(placesLived=>{
+let dt = document.createElement("dt");
+dt.textContent=placesLived.place;
+document.querySelector("#places-lived").appendChild(dt);
+
+let dd = document.createElement("dd");
+dd.textContent=placesLived.length;
+document.querySelector("#places-lived").appendChild(dd);
+});
+
